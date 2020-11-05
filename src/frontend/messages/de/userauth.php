a2aa872056c6399365388dce531f8f9df3a6db00
Basil Suter <git@nadar.io>
2020-11-05 09:59:19 +0000

    help text

--- src/frontend/messages/de/userauth.php
+++ src/frontend/messages/de/userauth.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'userauth.propertie.userauthprotection.label' => 'Benutzer-Authentifizierung aktivieren',
+    'userauth.propertie.userauthprotection.label' => 'Benutzer-Authentifizierung',
     'userauth.models.user.username' => 'Benutzername',
     'userauth.models.user.password' => 'Passwort',
     'userauth.models.user.password_salt' => 'Passwort salt',
@@ -10,6 +10,6 @@ return [
 
     // 1.1
     'userauth.propertie.selecteduserauthprotection.label' => 'Benutzer einschränken',
-    'userauth.propertie.selecteduserauthprotection.hint' => 'This is an additional restriction and only has an effect if page protection is enabled.',
-    'userauth.propertie.userauthprotection.hint' => 'Protects the page so only logged in users can see this page.',
+    'userauth.propertie.selecteduserauthprotection.hint' => 'Ermöglicht das einschränken der Seite nur für bestimmte Benutzer. Die Benutzer-Authentifizierung muss angeschalten sein damit diese Eigenschaft einen effekt hat.',
+    'userauth.propertie.userauthprotection.hint' => 'Wenn aktiviert, wird die Seite nur für eingeloggt Besucher einsehbar.',
 ];
